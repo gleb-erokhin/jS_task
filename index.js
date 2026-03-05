450cc19e023c52a2ab3355ee956604037e18d520
gleb.erokhin <gleb.erokhin@gmail.com>
2026-03-05 06:42:15 +0300

    все кроме delete готово

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let tasks = []
+let completedTasks = []
 let completedTaskCount = 0
 
 /**
@@ -12,44 +13,53 @@ let completedTaskCount = 0
  */
 
 
-function completeTask(task) {
-    if (task.length !== 0) {
+function completeTask(tasks, index) {
+    if (tasks.length !== 0) {
+        let currentTask = tasks[index]
+        currentTask.isCompleted = true
+        currentTask.completedDate = new Date()
         completedTaskCount++
-        return task = ''
+        completedTasks.push(currentTask)
+        // return task = ''
     } else {
         return `Выполнено: "Задача отсутствует"`
     }
 }
-
-function deleteTask(task) {
-    if (task.length !== 0) {
+// разобарться с delete
+function deleteTask(task, index) {
+    if (tasks.length !== 0) {
         return task = ''
     } else {
-        return `Удаление: "Задача отсутствует"`
+        if (isCompleted === false) {
+            return 'Таска еще не выполнена'
+        } else {
+            tasks.splice(index, 1)
+        }
+        // return `Удаление: "Задача отсутствует"`
     }
 }
 
-function showTask(tasks) {
+function showTask(tasks, name = 'Задача') {
     if (tasks.length === 0) {
         // return `"Задача отсутствует"`
         console.log("Состояние: Задача отсутствует")
     } else {
         tasks.forEach((task, index) => {
-            console.log(`Задача ${index + 1}:`);
+            console.log(`${name} ${index + 1}:`);
             Object.entries(task).forEach(([key, value]) => {
                 console.log(`${key}: ${value}`);
             });
         });
     }
-}   
+}
 
-function setTask(title, description) {
+function setTask(title, description, isCompleted = false, completedDate = null) {
     task = {
         title,
         description,
-        isCompleted: false,
-        createdDate: new Date(),
-        completedDate: null
+        isCompleted,
+        createdDate: new Date('2026-03-02'),
+        completedDate
     };
     tasks.push(task)
 }
@@ -57,5 +67,14 @@ function setTask(title, description) {
 setTask('Купить', 'хлеб')
 setTask('зарядка', 'в 7 утра3')
 setTask('сварить', 'кофе')
-console.log(tasks)
+
+completeTask(tasks, 1)
+completeTask(tasks, 0)
+
+deleteTask(tasks, 1)
+deleteTask(tasks, 0)
+
 showTask(tasks)
+showTask(completedTasks, 'Задача завершена')
+console.log('completedTaskCount:', completedTaskCount)
+console.log('completedTasks:', completedTasks)
