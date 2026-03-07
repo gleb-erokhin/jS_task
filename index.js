17c14f1c6c4252fb5db86d39c4af95b0fc329f3f
gleb.erokhin <gleb.erokhin@gmail.com>
2026-03-07 12:42:08 +0300

    произведены коментарии для понимания кода, доработаы ошибки, удалены лишние строки

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-let tasks = []
-let completedTasks = []
-let completedTaskCount = 0
+let tasks = [] // создаем пустой массив
+let completedTasks = [] // пустой массив для выполненых задач
+let completedTaskCount = 0 // счетчик выполненых задач
 
 /**
+ * пример задачи в массиве
  * const task = {
+ *  id: id
     title: "Купить продукты",
     description: "Молоко, хлеб, яйца",
     isCompleted: false,
@@ -12,22 +14,23 @@ let completedTaskCount = 0
 };
  */
 
-
+// функция выполнения задач, поиск по id
 function completeTask(id) {
+    // ищем индекс нужного объекта в массиве по id
     const index = tasks.findIndex(task => task.id === id);
-    // console.log('index find id', index, task.id)
+    // проверяем если индекс найден то выполняем код
     if (index !== -1) {
         let currentTask = tasks[index]
         currentTask.isCompleted = true
         currentTask.completedDate = new Date()
         completedTaskCount++
         completedTasks.push(currentTask)
-        // return task = ''
     } else {
         return `Выполнено: "Задача отсутствует"`
     }
 }
 
+// удаление задачи по id
 function deleteTask(id) {
     const index = tasks.findIndex(task => task.id === id);
     const task = tasks[index]; // получаем нужный объект из массива
@@ -46,13 +49,16 @@ function deleteTask(id) {
     }
 }
 
+// показываем массив с задачами, в атрибуты передаем массив, как основной так и с выполнеными задачами, необязательный параметр нужен для изменения вывода в консоль при необходимости
 function showTask(tasks, name = 'Задача') {
     if (tasks.length === 0) {
-        // return `"Задача отсутствует"`
         console.log("Состояние: Задача отсутствует")
     } else {
+        // пробегаемся по массиву, и заходя в каждый объект через анонимную функцию которая обрабатывает каждую задачу task 
         tasks.forEach((task) => {
+            // создаем заголовок для разделения отображения каждого объекта
             console.log(`${name} id ${task.id}:`);
+            // функция преобразовываем объект task в массив пар ключ-значение, и с помощью консоли выводим на экран
             Object.entries(task).forEach(([key, value]) => {
                 console.log(`${key}: ${value}`);
             });
@@ -61,10 +67,12 @@ function showTask(tasks, name = 'Задача') {
     }
 }
 
+// удалить все задачи
 function clearTasks(tasks) {
     tasks.length = 0
 }
 
+// добавить новую задачу
 function setTask(id, title, description, isCompleted = false, completedDate = false) {
     task = {
         id,
@@ -86,14 +94,15 @@ completeTask(2)
 // completeTask(1)
 
 
-// deleteTask(3)
-// deleteTask(2)
+deleteTask(3)
+deleteTask(2)
 // deleteTask(1)
 
 console.log('')
-console.log("выводим массив с новыми задачами")
+console.log("выводим массив с текущими задачами")
 
-console.log('') // раскоментировать чтобы очистить весь массив task, закоментировать delete, тогда он удалит все задачи
+console.log('') 
+// раскоментировать чтобы очистить весь массив task, закоментировать delete, тогда он удалит все задачи
 // clearTasks(tasks)
 showTask(tasks)
 
@@ -102,7 +111,7 @@ console.log('Выводим массив завершенные задачи')
 showTask(completedTasks, 'Задача завершена')
 
 
-console.log('')
+// console.log('')
 console.log('Выводим количество завершенных задач')
 console.log('completedTaskCount:', completedTaskCount)
 console.log('Выводим массив completedTasks:', completedTasks)
