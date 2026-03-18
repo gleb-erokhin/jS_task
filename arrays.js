2086f8d9778fa22cb6bcbd1e315615d5834a8766
gleb.erokhin <gleb.erokhin@gmail.com>
2026-03-18 06:41:59 +0300

    решение задач

--- arrays.js
+++ arrays.js
@@ -1,5 +1,35 @@
+//1
 let fruits = ["apple", "banana", "orange"];
 // Вывести первый и последний элемент
 // Изменить второй элемент на "grape" и вывести весь массив
-console.log(fruits[0])
-console.log(fruits.at(-1))
+// console.log(fruits[0])
+// console.log(fruits.at(-1))
+
+//2
+let numbers = [10, 20, 30];
+// Вывести длину
+numbers[5] = 50;
+// console.log(numbers.length)
+// Вывести массив и длину
+numbers.length = 3;
+// console.log(numbers.length)
+// Вывести массив и длину после усечения
+
+//3
+let arr3 = [];
+arr3[0] = "first";
+arr3[3] = "fourth";
+
+// Вывести весь массив
+// console.log(arr3)
+// // Вывести длину массива
+// console.log(arr3.length)
+// // Вывести третий элемент (arr3[2])
+// console.log(arr3[2])
+
+//4
+let arr4 = [5, 10, 15, 20];
+// Вывести последний элемент с использованием length
+// console.log(arr4[arr4.length - 1])
+// Попытаться вывести через отрицательный индекс arr[-1] и объяснить результат
+// console.log(arr4.at(-1)) // метод at дает доступ к использованию отрицательных индексов
