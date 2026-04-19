7c3a011bf00dc7c6367f5e227c231784cce2a476
gleb.erokhin <gleb.erokhin@gmail.com>
2026-04-19 12:59:24 +0300

    добавление заданий map

--- Современные структуры данных и обработка ошибок/работа в консоли/SetMap/SetMap.js
+++ Современные структуры данных и обработка ошибок/работа в консоли/SetMap/SetMap.js
@@ -1,3 +1,5 @@
+console.log('map')
+
 // 1. Создание Set с повторяющимися элементами
 const nums = new Set([1, 2, 2, 3, 3, 4]);
 console.log(nums); // ? [1, 2, 3, 4]
@@ -47,3 +49,52 @@ const setObjs = new Set([obj1, obj2, obj1]);
 console.log(setObjs.size); // ? 2 так как obj1 по ссылке это один объект
 console.log(setObjs.has(obj1)); // ? true
 console.log(setObjs.has({ a: 1 })); // ? false сравнение по ссылке а не по значению
+
+console.log('map')
+// 1. Создание Map и чтение значения
+const roles = new Map([['Alice', 'admin'], ['Bob', 'editor']]);
+console.log(roles.get('Alice')); // ? admin
+console.log(roles.get('Eve'));   // ? undefined
+
+// 2. Проверка существования ключа
+console.log(roles.has('Bob')); // ? true
+console.log(roles.has('Eve')); // ? false
+
+// 3. Добавление и обновление
+roles.set('Eve', 'viewer');
+roles.set('Alice', 'superadmin');
+console.log([...roles]); // ? ['Alice', 'superadmin'], ['Bob', 'editor'], ['Eve', 'viewer'], ]
+
+// 4. Удаление ключа
+roles.delete('Bob');
+console.log([...roles]); // ? [['Alice', 'superadmin'], ['Eve', 'viewer']]
+
+// 5. Размер Map
+console.log(roles.size); // ? 2
+
+// 6. Итерация for...of по Map
+for (const [key, value] of roles) {
+    console.log(key, value); // ? 'Alice', 'superadmin' n/ 'Eve', 'viewer'
+}
+
+// 7. Итерация forEach по Map
+roles.forEach((v, k) => console.log(k, v)); // ? 'Alice', 'superadmin' n/ 'Eve', 'viewer'
+
+// 8. Map из объекта
+const obj = { name: 'Alice', age: 25 };
+const mapFromObj = new Map(Object.entries(obj));
+console.log(mapFromObj.get('name')); // ? Alice
+console.log(mapFromObj.get('age'));  // ? 25
+
+// 9. Map в объект
+const map = new Map([['name', 'Bob'], ['age', 30]]);
+const objFromMap = Object.fromEntries(map);
+console.log(objFromMap); // ? { 'name', 'Bob', 'age', 30 }
+
+// 10. Map с объектами в качестве ключей
+const u1 = { id: 1 }, u2 = { id: 2 };
+const scores = new Map();
+scores.set(u1, 100);
+scores.set(u2, 200);
+console.log(scores.get(u1)); // ? 100 
+console.log(scores.get({ id: 1 })); // ? undefined потому что объект добавляется по содержимому а не по ссылке
